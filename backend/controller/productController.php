1d249057b403e2f9b5e91e0ba874c68390c52479
ammar <45490495+3mmar7amed@users.noreply.github.com>
2022-01-24 17:26:47 +0200

    c

--- backend/controller/productController.php
+++ backend/controller/productController.php
@@ -26,7 +26,6 @@ class productController
 
 
     public function setProducts() {
-        echo "here is to post" ;
         $requestData = json_decode(file_get_contents('php://input'), true);
         $product = new products() ;
         $product->setProducts($requestData);
