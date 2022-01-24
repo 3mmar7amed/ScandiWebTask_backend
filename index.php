afc8a91c46c223fcd297e6c3552ab0485c8411d1
ammar <45490495+3mmar7amed@users.noreply.github.com>
2022-01-24 14:27:39 +0200

    heroku create

--- index.php
+++ index.php
@@ -9,7 +9,7 @@ $router = new Router() ;
 
 
 $router->get('/', [new productController(), 'getProducts'] );
-$router->get("/products", [new productController(), 'getProducts']);
+$router->get('/products', [new productController(), 'getProducts']);
 $router->post('/addproduct', [new productController(), 'setProducts']);
 $router->delete('/Delete', [new productController(), 'deleteProducts']);
 
